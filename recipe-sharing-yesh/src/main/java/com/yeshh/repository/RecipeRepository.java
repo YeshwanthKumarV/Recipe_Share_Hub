5e75ff734aebb7f5a07bb3a129a1842b8dfe5dcb
YeshwanthKumarV <149353044+YeshwanthKumarV@users.noreply.github.com>
2024-01-29 21:22:01 +0530

    Delete recipe-sharing-yesh directory

--- recipe-sharing-yesh/src/main/java/com/yeshh/repository/RecipeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yeshh.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.yeshh.model.Recipe;
-
-public interface RecipeRepository extends JpaRepository<Recipe,Long> {
-
-}
